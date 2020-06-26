464e1a185e4047db7939d3317041146d3b372536
SandhyaG-15 <67462383+SandhyaG-15@users.noreply.github.com>
2020-06-26 15:39:58 +0530

    Create getintegerwithdigitscount.c

---
+++ getintegerwithdigitscount.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+  int x=0,i=0,cnt=0,count=1;
+  int n=100000000;
+  while(n!=0){
+    arr[i++]=n%10;
+    cnt+=1;
+    n/=10;
+    }
+  for(i=cnt-1;i>=0;i--){
+  if(arr[i]==arr[i-1] && i-1!=0)
+    count+=1;
+  else{
+    x=x*10+count;
+    x=x*10+arr[i];
+    cnt-=count;
+    count=1;
+    }
+  if(cnt>0){
+    x=x*10+count;
+    x=x*10+arr[i];
+   }
+  printf("%d",x);
+}
